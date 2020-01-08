e4c558244bc6220186c3e6c9186b76136e73bc86
Xinyu Chen <xinyuchen2769@gmail.com>
2020-01-07 19:22:13 -0500

    implement half of the functions

--- src/main/java/Collections/MapPractice.java
+++ src/main/java/Collections/MapPractice.java
@@ -1,15 +1,25 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
 public class MapPractice {
 
     public Object findValueOf(Map map, Object key) {
-        return null;
+        return map.get(key);
     }
 
     public Object[] findKeysOf(Map map, Object value) {
-        return null;
+        ArrayList<Object> list = new ArrayList<Object>();
+        Iterator ite = map.entrySet().iterator();
+        while(ite.hasNext()){
+            Map.Entry en = (Map.Entry) ite.next();
+            if(en.getValue().equals(value)){
+                list.add(en.getKey());
+            }
+        }
+        return list.toArray();
     }
 
     public Map<Integer, Integer> fibonacciTree(int size) {
--- src/main/java/Strings/BasicStrings.java
+++ src/main/java/Strings/BasicStrings.java
@@ -3,26 +3,41 @@ package Strings;
 public class BasicStrings {
 
     public String flipConcat(String string1, String string2) {
-        return null;
+        return string2 + string1;
     }
 
     public char getChar(String string, int index) {
-        return ' ';
+        return string.charAt(index);
     }
 
     public String iCantSee(String string) {
-        return null;
+        String str = "";
+        for(int i = 0; i < string.length(); i++){
+            str = str + " ";
+        }
+        return str;
     }
 
     public String loudAndClear(String string) {
-        return null;
+        return string.toUpperCase();
     }
 
     public String reverseCase(String string) {
-        return null;
+        String str = "";
+        for(int i = 0; i < string.length(); i++){
+            if(Character.isUpperCase(string.charAt(i))) {
+                str += Character.toLowerCase(string.charAt(i));
+            }
+            else if(Character.isLowerCase(string.charAt(i))){
+                str += Character.toUpperCase(string.charAt(i));
+            }
+        }
+        return str;
     }
 
     public String oneAtATime(String string1, String string2) {
-        return null;
+        int i1 = 0;
+        int i2 = 0;
+
     }
 }
